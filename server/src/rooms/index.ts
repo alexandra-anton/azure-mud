a07321f065e8029f5b4296abbb1498e874f063b8
Cristian Sosu <cristian.sosu@netcentric.biz>
2020-12-14 23:03:01 +0200

    export secretRoom

--- server/src/rooms/index.ts
+++ server/src/rooms/index.ts
@@ -2,7 +2,7 @@ import northShowcaseHall from './northShowcaseHall'
 import southShowcaseHall from './southShowcaseHall'
 import eastShowcaseHall from './eastShowcaseHall'
 import westShowcaseHall from './westShowcaseHall'
-import { unconference, minetown, oracle, tower, castle, sokoban, astralPlane, phoneBooth1, phoneBooth2, bran } from './unconfRooms'
+import { unconference, minetown, oracle, tower, secretRoom, castle, sokoban, astralPlane, phoneBooth1, phoneBooth2, bran } from './unconfRooms'
 import theater from './theater'
 import { loungeDungeonRoomData } from './loungeDungeon'
 
