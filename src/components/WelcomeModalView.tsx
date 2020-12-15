fdc4ffb5c070c375fdc94e70c2aca90936182a16
Alex Dragu <alex.dragu@netcentric.biz>
2020-12-15 20:29:19 +0200

    Added :tada: emoji

--- src/components/WelcomeModalView.tsx
+++ src/components/WelcomeModalView.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function WelcomeModalView(props: {}) {
   return (
     <div>
-      <p>Welcome to the Netcentric virtual studio :tada: </p>
+      <p>Welcome to the Netcentric virtual studio <span role="img" aria-label="tada">🎉</span> </p>
       <p>Many of us wondered when we will be able to return to the office. We did some redecoration in the meantime and now it's ready again.</p>
       <p>You can sip a coffee at the reception, take the elevator to a cigarette break, talk to other colleagues, explore unknown corners and interact with your surroundings.</p>
       <p>Talk to each other off Slack and make this Christmas celebration feel a little more like a real-world interaction. </p>
