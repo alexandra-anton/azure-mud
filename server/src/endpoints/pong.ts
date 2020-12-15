e6b3f524e18b1086f3762f0f27dd3962e3bdd7cd
Cristian Sosu <cristian.sosu@netcentric.biz>
2020-12-15 22:44:13 +0200

    move pong to 501 to test issue origin

--- server/src/endpoints/pong.ts
+++ server/src/endpoints/pong.ts
@@ -5,7 +5,7 @@ const pong: EndpointFunction = async (inputs: any, log: LogFn) => {
   if (!inputs.userId) {
     return {
       httpResponse: {
-        status: 500,
+        status: 501,
         body: 'You did not include a user ID'
       }
     }
