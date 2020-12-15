4f255f38dfe6729366f0da3bb1dba1ab044a9427
Alex Dragu <alex.dragu@netcentric.biz>
2020-12-15 14:39:47 +0200

    Updated entry page text

--- src/components/LoggedOutView.tsx
+++ src/components/LoggedOutView.tsx
@@ -1,55 +1,46 @@
 import React from 'react'
 import config from '../config'
 
-export default function LoggedOutView () {
+export default function LoggedOutView() {
   return (
     <div>
       <header role="banner">
-        <h1>A Christmas story in Covid times</h1>
+        <h1>A Christmas story in Covid times 🎅</h1>
       </header>
       <main role="main">
         <p>
           This is a social space for attendees of{' '}
-          <a href='https://roguelike.club'>Christmas story in Covid times</a>, a
-          community-generated weekend of talks, games, and conversations about
-          roguelikes and related topics, including procedural generation and
-          game design. It&apos;s for fans, players, developers, scholars, and
-          everyone else!
+          <a href='#'>Christmas story in Covid times</a>, a place where we can feel as close to our dear, very missed studio as possible. Feel free to click on the login link below so you can explore the the Xmas transforrmed studio and interact with our lovely coleagues.
         </p>
         <a
-          href={`${
-            config.SERVER_HOSTNAME
-          }/.auth/login/google?post_login_redirect_url=${encodeURIComponent(
-            window.location.href
-          )}`}
+          href={`${config.SERVER_HOSTNAME
+            }/.auth/login/google?post_login_redirect_url=${encodeURIComponent(
+              window.location.href
+            )}`}
         >
           Log In With Google
-        </a><br/>
+        </a><br />
         {(window as any).safari
           ? <p>
-            <strong style={{ color: 'red' }}> You appear to be using Safari.</strong> <br/>
+            <strong style={{ color: 'red' }}> You appear to be using Safari.</strong> <br />
             If you are having difficulty logging in, you may need to allow third-party cookies
             <ul>
-              <li>On macOS, Safari -&gt; Preferences -&gt; uncheck "Prevent cross-site tracking"</li>
-              <li>On iOS, Settings -&gt; Safari -&gt; uncheck "Prevent cross-site tracking"</li>
+              <li>On macOS, Safari -&gt; Preferences -&gt; uncheck &lt;Prevent cross-site tracking&gt;</li>
+              <li>On iOS, Settings -&gt; Safari -&gt; uncheck &lt;Prevent cross-site tracking&gt;</li>
             </ul>
-            We know this is sketchy! We promise we're not capturing or selling data to any third party, it's just a limitation of the way we're handling logins.<br/>
-            If you're not comfortable with this, feel free to switch to Chrome, Firefox, or another non-Safari browser on your Mac.
+            We know this is sketchy! We promise we&apos;re not capturing or sharing data to any third party, it&apos; just a limitation of the way we&apos;re handling logins.<br />
+            If you&apos;re not comfortable with this, feel free to switch to Chrome, Firefox, or another non-Safari browser on your Mac.
           </p> : null
         }
         <p>
-          No matter which service you log in with, we do not access any data
+          <b>Note: </b>We do not access any data
           other than what is needed to authenticate you. If it would make you
           more comfortable, feel free to sign up for and use a throwaway
-          account on one of these services.
-        </p>
-        <p>
-          After logging in, you will have the opportunity to pick whatever chat handle you would
-          like before entering the space.
+          account on Google. Just make sure that others can identify you ¯\_(ツ)_/¯
         </p>
         <p>
           By entering the space, you agree to our{' '}
-          <a href={'https://roguelike.club/code.html'}>Code of Conduct</a>.
+          <a href={'https://projects.netcentric.biz/wiki/display/NETCENTRIC/Core+Values'}>Code of Conduct</a>.
         </p>
       </main>
     </div>
