dbcaf3c3065c32537cf0f1c12847ef1a11e37bb4
Alex Dragu <alex.dragu@netcentric.biz>
2020-12-16 20:29:29 +0200

    typo

--- src/components/CodeOfConductView.tsx
+++ src/components/CodeOfConductView.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CodeOfConductView() {
   return (
     <div id='code-of-conduct'>
-      <p>Our goal is to have a fun and get the studio feeling back into our lives during the WFH temporary situation. Having said that, please act around this virtual studio just as you would do when in the real office. Here is a quick reminder of our <a href='https://projects.netcentric.biz/wiki/display/NETCENTRIC/Core+Values' target='_blank' rel='noreferrer'>Core Values.</a></p>
+      <p>Our goal is to have fun and get the studio feeling back into our lives during the WFH temporary situation. Having said that, please act around this virtual studio just as you would do when in the real office. Here is a quick reminder of our <a href='https://projects.netcentric.biz/wiki/display/NETCENTRIC/Core+Values' target='_blank' rel='noreferrer'>Core Values.</a></p>
       <p>If you&apos;re being harassed, notice someone being harassed, or have any other concerns related to the code of conduct, you have a few options:</p>
       <ul>
         <li>Point out the mistake to the offender directly as they might not have bad intentions. You can use <code>/whisper</code> into the chat box, followed by a message, to contact them directly or our Netcentric tools like Slack.</li>
