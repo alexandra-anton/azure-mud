2dd23838a87653c6d69b22604293fbedc42f76d2
Alexandra Anton <alexandra.anton@netcentric.biz>
2020-12-16 20:50:03 +0200

    schedule and sys eng

--- server/src/rooms/southShowcaseHall.ts
+++ server/src/rooms/southShowcaseHall.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'Peretele de pluta',
   shortName: 'the cork wall',
   description: `
-  You arrive at the cork wall and look at all the <a href="google-photos-memories.html" onClick="window.open(\'google-photos.html#\' + window.getComputedStyle(document.body).getPropertyValue(\'background-color\'), \'google-photos-memories\', \'width=560,height=460\'); return false">pictures</a> of you and your colleagues at different events or simple snapshots of day-to-day office life. Some of them are corny, but still, they manage to spark in you a sense of joy towards the memories you have here and the ones that are yet to come.
+  You arrive at the cork wall and look at all the <a href="google-photos-memories.html" onClick="window.open(\'google-photos-memories.html#\' + window.getComputedStyle(document.body).getPropertyValue(\'background-color\'), \'google-photos-memories\', \'width=560,height=460\'); return false">pictures</a> of you and your colleagues at different events or simple snapshots of day-to-day office life. Some of them are corny, but still, they manage to spark in you a sense of joy towards the memories you have here and the ones that are yet to come.
   <br/><br/>
   
   You can go towards the [[Reception->foyer]], pay a visit to the Sys Eng corner or join everybody else in the MM area.`,
