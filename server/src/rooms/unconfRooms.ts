827480b787ead8b3d72d1dab81760104ccb199e2
Alex Dragu <alex.dragu@netcentric.biz>
2020-12-16 19:12:38 +0200

    Afara la tigara - added target blank for wonder link Tineretului - added text and images for the board games - also added Impressions Board.

--- server/src/rooms/unconfRooms.ts
+++ server/src/rooms/unconfRooms.ts
@@ -10,11 +10,11 @@ export const unconference = {
   allowsMedia: true,
   hasNoteWall: true,
   noteWallData: {
-    roomWallDescription: 'Ideas and comments about A Christmas Story in Covid times - Netcentric Bucharest Studio Edition!',
-    noteWallButton: 'Impressions Board',
-    addNoteLinkText: 'add an impression',
-    addNotePrompt: 'What would you like to say?',
-    noteWallDescription: `How did you like our virtual event? Add you impression here. 
+    roomWallDescription: '',
+    noteWallButton: 'Add Game Ideas',
+    addNoteLinkText: 'add a game idea',
+    addNotePrompt: 'What would you like to recommend?',
+    noteWallDescription: `What online board games are you playing or would like to play? 
     `
   }
 }
@@ -24,7 +24,7 @@ export const minetown = {
   displayName: 'Afara la Tigara',
   shortName: 'Afara la Tigara',
   description: `<p>You notice a lot of people either walking in circles while being in conf calls on bluetooth or frenetically tapping emails on their mobile phones. Some of them are here since your first break in the morning, they seem to be the definition of mobile office and probably already sub-rented their desk upstairs.<br /><br />You decide to light up [[a halfway done cigarette->item]] or try to revive [[an iqos with no battery->item]].</p>
-  Usual gossip / complaining chats go <a href="https://www.wonder.me/r?id=82bd1aea-acd3-4133-93e8-c7ae7c73500d">&lt;here&gt;</a> (pass: <em>xmasstory</em>)
+  Usual gossip / complaining chats go <a href="https://www.wonder.me/r?id=82bd1aea-acd3-4133-93e8-c7ae7c73500d" target="_blank">&lt;here&gt;</a> (pass: <em>xmasstory</em>)
   <br/><br/>From here, you can go back to the [[Elevator B->lounge]] or take the stairs directly to [[Reception->foyer]]`,
   hasNoteWall: true
 }
